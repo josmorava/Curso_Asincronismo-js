92ba416ed0d19fa4b02907cac0cdddb32ffb3b92
josmorava <josemorales-1234@hotmail.es>
2020-11-06 03:57:54 -0400

    Terminando Challenge

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,5 +1,6 @@
 //Reto de la api de Rick and Morty
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+let API = 'https://rickandmortyapi.com/api/character/';
 
 function fetchData(url_api, callback){
   let xhttp = new XMLHttpRequest();
@@ -17,3 +18,18 @@ function fetchData(url_api, callback){
   }
   xhttp.send(); //Funcion que dfa vida a las llamadas de la api
 }
+fetchData(API, function(error1, data1){
+  if (error1) return console.error(error1);
+  fetchData(API + data1.results[0].id, function(error2,data2){
+    if(error2) return console.error(error2);
+    fetchData(data2.origin.url, function(error3, data3){
+      if(error3) return console.error(error3);
+      console.log(data1.info.count);
+      console.log(data2.name);
+      console.log(data3.dimension);
+    });
+  })
+})
+//Forma de encadenar callbacks para traer información precisa de un personaje, no se pueden traer de una sola llamada ya que la API no lo permite - Esto se conoce como calback hell!
+
+//Peticiones a la api, y obetener los datos necesarios mediante callbacks
